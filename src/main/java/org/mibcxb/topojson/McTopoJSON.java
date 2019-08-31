dea7239d235862d228b37814abfb1b3ef61782fc
situx <timo.homburg@gmx.de>
2019-09-01 01:42:41 +0200

    *Updated JTS dependency

--- src/main/java/org/mibcxb/topojson/McTopoJSON.java
+++ src/main/java/org/mibcxb/topojson/McTopoJSON.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 import org.mibcxb.McException;
 
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.LinearRing;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.LinearRing;
 
 public class McTopoJSON {
     private ITopoJSONHandler handler;
